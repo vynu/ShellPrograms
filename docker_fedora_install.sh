5e840b03b58bad5cece87ac04d8f1d206dac94f1
vynu <vynu@live.com>
2017-09-15 02:43:43 -0400

    modified docker fedora install shell

--- docker_fedora_install.sh
+++ docker_fedora_install.sh
@@ -5,8 +5,9 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager --set-enabled docker-ce-edge
 sudo dnf makecache
-sudo dnf install docker-ce
+sudo dnf -y install docker-ce
 sudo systemctl start docker
 sudo usermod -aG docker fedora
 
